443b44c7e38b6965df2757ebeac70f5f57b38b7e
olaide-hok <olaidekareem43@yahoo.com>
2022-05-21 17:15:24 +0100

    Fetch Questions

--- src/context.js
+++ src/context.js
@@ -25,6 +25,31 @@ const AppProvider = ({ children }) => {
 
   const [isModalOpen, setIsModalOpen] = useState(false)
 
+  const fetchQuestions = async (url) => {
+    setLoading(true)
+    setWaiting(false)
+    const response = await axios.get(url).catch((error) => 
+    console.log(error))
+    if (response) {
+      const data = response.data.results
+      if (data.length > 0) {
+        setQuestions(data)
+        setLoading(false)
+        setWaiting(false)
+        setError(false)
+      } else {
+        setWaiting(true)
+        setError(true)
+      }
+    }
+    else {
+      setWaiting(true)
+    }
+  }
+
+    useEffect(() => {
+      fetchQuestions(tempUrl)
+    }, [])
 
   return <AppContext.Provider value={{
     waiting,
