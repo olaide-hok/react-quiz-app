27ed2012499c853a09f8b8134c6e5fe63e549f94
olaide-hok <olaidekareem43@yahoo.com>
2022-05-22 14:08:53 +0100

    Display Questions

--- src/App.js
+++ src/App.js
@@ -14,8 +14,32 @@ function App() {
   if (loading) {
     return <Loading />
   }
+
+  const { question, incorrect_answers, correct_answer} = questions[index]
+  const answers = [...incorrect_answers, correct_answer]
+
   return <main>
-    Quiz App
+    {/* <Modal /> */}
+    <section className='quiz'>
+      <p className='correct-answers'>
+        correct answers : {correct}/{index}
+      </p>
+      <article className='conntainer'>
+        <h2 dangerouslySetInnerHTML={{ __html: question }} />
+        <div className='btn-container'>
+          {answers.map((answer, index) => {
+            return (
+              <button 
+                key={index}
+                className='answer-btn'
+                dangerouslySetInnerHTML={{ __html: answer}}
+              />
+            )
+          } )}
+        </div>
+      </article>
+      <button className='next-question'>next question</button>
+    </section>
   </main>
 }
 
