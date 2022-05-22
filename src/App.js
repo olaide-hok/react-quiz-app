7f68bc34ed0985c6e38410fa6c027fff0830d2c7
olaide-hok <olaidekareem43@yahoo.com>
2022-05-22 15:15:14 +0100

    Setup next question feature

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
 function App() {
-  const {waiting, loading, questions, index, correct} = useGlobalContext()
+  const {waiting, loading, questions, index, correct, nextQuestion} = useGlobalContext()
 
   if(waiting) {
     return <SetupForm />
@@ -38,7 +38,9 @@ function App() {
           } )}
         </div>
       </article>
-      <button className='next-question'>next question</button>
+      <button 
+        onClick={nextQuestion}
+        className='next-question'>next question</button>
     </section>
   </main>
 }
