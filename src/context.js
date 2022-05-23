101c8912bf28b8b0a5246b018e73db4b6b8dc1a4
olaide-hok <olaidekareem43@yahoo.com>
2022-05-23 08:40:48 +0100

    Randomize correct answers

--- src/context.js
+++ src/context.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext } from 'react'
 
 const table = {
   sports: 21,
@@ -8,9 +8,6 @@ const table = {
 }
 
 const API_ENDPOINT = 'https://opentdb.com/api.php?'
-
-const url = `${API_ENDPOINT}`
-const tempUrl = 'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
 
 const AppContext = React.createContext()
 
