d7f8ff8707bde3daf20bdce4ed732c3ff61f4de6
olaide-hok <olaidekareem43@yahoo.com>
2022-05-23 08:03:04 +0100

    Setup Form Structure

--- src/context.js
+++ src/context.js
@@ -22,6 +22,11 @@ const AppProvider = ({ children }) => {
   const [index, setIndex] = useState(0)
   const [correct, setCorrect] = useState(0)
   const [error, setError] = useState(false)
+  const [quiz, setQuiz] = useState({
+    amount: 10,
+    category: 'sports',
+    difficulty: 'easy'
+  })
 
   const [isModalOpen, setIsModalOpen] = useState(false)
 
@@ -76,6 +81,14 @@ const AppProvider = ({ children }) => {
     setIsModalOpen(false)
   }
 
+  const handleChange = (e) => {
+    console.log(e);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
     useEffect(() => {
       fetchQuestions(tempUrl)
     }, [])
@@ -90,7 +103,10 @@ const AppProvider = ({ children }) => {
     isModalOpen,
     nextQuestion,
     checkAnswer,
-    closeModal
+    closeModal,
+    quiz,
+    handleChange,
+    handleSubmit
 
   }}>{children}</AppContext.Provider>
 }
